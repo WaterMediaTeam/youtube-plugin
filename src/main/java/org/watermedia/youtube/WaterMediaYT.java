01b7a7a09fd8e8baf576dcb7895698b072d470dc
J-RAP <srrapero720@hotmail.com>
2026-04-27 02:41:23 -0600

    Add delay on register

--- src/main/java/org/watermedia/youtube/WaterMediaYT.java
+++ src/main/java/org/watermedia/youtube/WaterMediaYT.java
@@ -1,17 +1,27 @@
 package org.watermedia.youtube;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.schabi.newpipe.extractor.NewPipe;
 import org.schabi.newpipe.extractor.localization.Localization;
+import org.watermedia.WaterMedia;
 import org.watermedia.api.media.MediaAPI;
+import org.watermedia.tools.ThreadTool;
 import org.watermedia.youtube.patch.YouTubePlatform;
 
 import java.util.Locale;
 
 public class WaterMediaYT {
     public static final String ID = "watermedia_youtube_extension";
+    public static final Logger LOGGER = LogManager.getLogger(ID);
 
     public static void start() {
+        LOGGER.info("Registering YT extension");
         NewPipe.init(new YouTubePlatform.WaterMediaDownloader(), Localization.fromLocale(Locale.getDefault()));
-        MediaAPI.registerPlatform(new YouTubePlatform(), org.watermedia.api.media.platform.YoutubePlatform.class);
+        // DIRTY HACK BECAUSE LOADERS DOESN'T ORDER LOADING PROPERLY
+        ThreadTool.createStarted("Register new Youtube", () -> {
+            ThreadTool.sleep(15000);
+            MediaAPI.registerPlatform(new YouTubePlatform(), org.watermedia.api.media.platform.YoutubePlatform.class);
+        });
     }
 }
